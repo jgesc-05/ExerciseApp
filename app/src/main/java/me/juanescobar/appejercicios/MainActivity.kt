0194068469b5d28514bb82ff190f80aa3020adb0
Juan Escobar <jescobar435@unab.edu.co>
2025-05-07 16:54:39 -0500

    Volvi a ajustar la navegacion

--- app/src/main/java/me/juanescobar/appejercicios/MainActivity.kt
+++ app/src/main/java/me/juanescobar/appejercicios/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import me.juanescobar.appejercicios.ui.theme.AppEjerciciosTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,31 +22,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppEjerciciosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val myNavController = rememberNavController()
+                val myStartDestination = "login"
+
+                NavHost(
+                    navController = myNavController,
+                    startDestination = myStartDestination
+                ) {
+                    composable("login") {
+                        LoginScreen(myNavController)
+                    }
+
+                    composable("register") {
+                        RegisterScreen(myNavController)
+                    }
+
+                    composable("survey") {
+                        SurveyScreen(myNavController)
+                    }
+
+                    composable("home") {
+                        HomeScreen(myNavController)
+                    }
                 }
             }
 
-            }
-       }
-
+        }
     }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppEjerciciosTheme {
-        Greeting("Android")
-    }
-}
